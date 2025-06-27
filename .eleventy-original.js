aa4bda39c18ed8139c740003826b1ccd415eb802
Samuel Mayworm <87725123+mayworms@users.noreply.github.com>
2025-06-26 20:21:46 -0400

    config file + footer javascript
    footer javascript == i don't have to manually update the year anymore

--- .eleventy-original.js
+++ .eleventy-original.js
@@ -1,31 +1,34 @@
-import { DateTime } from 'luxon';
+// Tells Eleventy to look for Luxon
+const { DateTime } = require('luxon');
 
-export default function(eleventyConfig) {
-  // Passthrough and watch targets
+// This is all the stuff that Eleventy is going to process when it exports your site
+module.exports = function (eleventyConfig) {
+  // Edit these to include your images, CSS, and other folders and files that you want to copy over to your public folder.
   eleventyConfig.addPassthroughCopy("./src/styles");
   eleventyConfig.addWatchTarget("./src/styles/");
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addWatchTarget("./src/images/");
 
-  // Adds Next & Previous links to the bottom of my blog posts
+  // Adds Next & Previous links to the bottom of our blog posts
   eleventyConfig.addCollection("posts", function(collection) {
-    const coll = collection.getFilteredByTag("posts");
-    for (let i = 0; i < coll.length; i++) {
-      const prevPost = coll[i - 1];
-      const nextPost = coll[i + 1];
+    const coll = collection.getFilteredByTag("posts");    
+    for(let i = 0; i < coll.length ; i++) {
+      const prevPost = coll[i-1];
+      const nextPost = coll[i + 1];      
       coll[i].data["prevPost"] = prevPost;
       coll[i].data["nextPost"] = nextPost;
     }
     return coll;
   });
 
-  // Date formatting
+  // Add the filter "readableDate" to simplify the way blog dates are presented in the Archives page
   eleventyConfig.addFilter('readableDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: 'utc+9' }).toFormat(
       'yyyy-LL-dd'
     );
   });
 
+  // Add the filter "topDate" to simplify the way blog dates are presented at the top of blog posts
   eleventyConfig.addFilter('topDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: 'utc+9' }).toFormat(
       'yyyy LLLL dd'
@@ -45,10 +48,12 @@ export default function(eleventyConfig) {
     "./samuelscode.js" : "/assets/js/samuelscode.js",
   });
 
+
+  // These are the folders that Eleventy will use. "src" is where you edit files that Eleventy will then take in and export into "public," which you upload.
   return {
     dir: {
       input: "src",
       output: "public",
     },
   };
-}
+};
